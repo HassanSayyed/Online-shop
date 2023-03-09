3f4f03d074af84dc3bd1737acfa522529770b34d
Hasan Al Sayyed <hasanalsayyed651998@gmail.com>
2023-03-09 16:06:16 +0200

    add request validation middleware  (Joi) , complete Products route: get by id , update by id ,delete by id

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,5 +1,6 @@
 const Product = require("../moduels/Product");
 const { StatusCodes } = require("http-status-codes");
+const mongoose = require("mongoose");
 
 const createProduct = async (req, res) => {
   const product = new Product({
@@ -31,7 +32,88 @@ const getAllProducts = async (req, res) => {
   }
 };
 
+const getProductById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    // check if {id} is valid mongoose id
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ error: "Invalid product id" });
+    }
+    const product = await Product.findById(id);
+    if (!product) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ error: "Product not found" });
+    }
+    res.status(StatusCodes.OK).json({ product });
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: error.message });
+  }
+};
+
+const updateProductById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    // check if {id} is valid mongoose id
+    if (!mongoose.isValidObjectId(id)) {
+      return res.status(StatusCodes.BAD_REQUEST).json({ error: "Invalid ID" });
+    }
+
+    const { name, price, image, description, category } = req.body;
+
+    const product = await Product.findByIdAndUpdate(
+      id,
+      { name, price, image, description, category },
+      { new: true }
+    );
+
+    if (!product) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ error: "Product not found" });
+    }
+
+    res.status(StatusCodes.OK).json({ product });
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: error.message });
+  }
+};
+
+const deleteProductById = async (req, res) => {
+  const { id } = req.params;
+  // check if {id} is valid mongoose id
+  if (!mongoose.isValidObjectId(id)) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ error: "Invalid product ID" });
+  }
+
+  try {
+    const deletedProduct = await Product.findByIdAndDelete(id);
+    if (!deletedProduct) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ error: "Product not found" });
+    }
+    // delete is successful send 204 (no content)
+    res.sendStatus(StatusCodes.NO_CONTENT);
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: error.message });
+  }
+};
+
 module.exports = {
   createProduct,
   getAllProducts,
+  getProductById,
+  updateProductById,
+  deleteProductById,
 };
--- server/routes/productsRoutes.js
+++ server/routes/productsRoutes.js
@@ -1,10 +1,32 @@
 const express = require("express");
+const Joi = require("joi");
+const validateRequest = require("../middleware/validation");
 const router = express.Router();
 const {
   createProduct,
   getAllProducts,
+  getProductById,
+  updateProductById,
+  deleteProductById,
 } = require("../controllers/productController");
 
-router.route("/").get(getAllProducts).post(createProduct);
+const productSchema = Joi.object({
+  name: Joi.string().required(),
+  price: Joi.number().required(),
+  image: Joi.string().uri(),
+  description: Joi.string(),
+  category: Joi.string(),
+});
+
+router
+  .route("/")
+  .get(getAllProducts)
+  .post(validateRequest(productSchema), createProduct);
+
+router
+  .route("/:id")
+  .get(getProductById)
+  .patch(validateRequest(productSchema), updateProductById)
+  .delete(deleteProductById);
 
 module.exports = router;
